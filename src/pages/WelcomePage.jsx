9c3d57709703f0d6210fdca2106ffaa7454c196c
Konson Ak <konakech3@gmail.com>
2023-10-14 21:45:19 +0300

    first commit

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function WelcomePage() {
   return (
-    <div className="h-screen welcome-page flex flex-col justify-end p-14">
+    <div className="h-screen welcome-page flex flex-col justify-end px-10 py-[6rem]">
         <h1 className="text-4xl font-bold">Enjoy the testies Cakes near you.</h1>
         <Link className="w-[max-content] bg-red-900 flex items-center text-white rounded px-5 py-3 mt-12" to='/home'>
             <span className="text-xl">Get Started</span>
