6b8e89c5e32484e893a667c609da920749c032ea
Konson Ak <konakech3@gmail.com>
2023-10-20 23:01:16 +0300

    update

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   // baseURL:process.env.REACT_APP_PROD_URL,
-  baseURL:process.env.REACT_APP_LOCAL_URL,
-  // baseURL:'https://leee3-api.onrender.com/'
+  // baseURL:process.env.REACT_APP_LOCAL_URL,
+  baseURL:'https://leee3-api.onrender.com/'
 })
 export default axiosInstance
 
