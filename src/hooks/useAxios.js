6e2324f84bd694bba01e2a81b1cb3e1ec2f44efb
Konson Ak <konakech3@gmail.com>
2023-10-26 03:33:52 +0300

    removed cred

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API,
-  withCredentials: true
+  // withCredentials: true
 })
 export default axiosInstance
 
