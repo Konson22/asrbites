75776e3c333bba15be670539f7f84ffe9db23fdc
Konson Ak <konakech3@gmail.com>
2023-10-27 16:26:49 +0300

    added

--- src/pages/StorePage.jsx
+++ src/pages/StorePage.jsx
@@ -32,7 +32,7 @@ export default function StorePage() {
         </div>
         <div className="content md:px-[5%] px-[3%] mt-8">
             <h3 className="text-2xl font-bold text-right">المنتجات</h3>
-            <div className="flex-1 grid md:grid-cols-4 grid-cols-2 gap-5 mt-6">
+            <div className="flex-1 grid md:grid-cols-4 grid-cols-2 md:gap-5 gap-2 mt-6">
                 {candy && candy.length > 0 && candy.map(item => <ItemCart item={item} key={item.id} />)}
             </div>
         </div>
